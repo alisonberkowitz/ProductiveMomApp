ba44894b2edc7bddeb2b6c002249514b4f0d92c0
Berko <alison.berkowitz@students.olin.edu>
2013-11-10 16:39:56 -0500

    added purple and yellow hex values to our colors

--- MommyAppProject/MommyApp/src/main/java/com/mobileproto/mommyapp/PersonDetailActivity.java
+++ MommyAppProject/MommyApp/src/main/java/com/mobileproto/mommyapp/PersonDetailActivity.java
@@ -1,7 +1,15 @@
 package com.mobileproto.mommyapp;
 
+import android.os.Bundle;
+
 /**
  * Created by alison on 11/10/13.
  */
 public class PersonDetailActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_detail);
+    }
 }
