7128274d8432acf56afbcd527c6987119d467fb1
Berko <alison.berkowitz@students.olin.edu>
2013-11-10 19:26:33 -0500

    making the person detail layout

--- MommyAppProject/MommyApp/src/main/java/com/mobileproto/mommyapp/PersonDetailActivity.java
+++ MommyAppProject/MommyApp/src/main/java/com/mobileproto/mommyapp/PersonDetailActivity.java
@@ -1,15 +1,23 @@
 package com.mobileproto.mommyapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 /**
  * Created by alison on 11/10/13.
  */
 public class PersonDetailActivity {
 
+    TextView personName;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person_detail);
+        Intent intent = getIntent();
+
+        personName = (TextView)findViewById(R.id.personName);
     }
 }
