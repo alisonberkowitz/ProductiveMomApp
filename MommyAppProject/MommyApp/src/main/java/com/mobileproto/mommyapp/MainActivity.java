7418d14fd4b0c7899d87ab3fbace276a9bed8c16
Chloe Vilain <chloe.vilain@students.olin.edu>
2013-11-11 12:03:57 -0500

    Chloe V latest updates

--- MommyAppProject/MommyApp/src/main/java/com/mobileproto/mommyapp/MainActivity.java
+++ MommyAppProject/MommyApp/src/main/java/com/mobileproto/mommyapp/MainActivity.java
@@ -17,14 +17,28 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //get the moms
+        Mom mom1 = getMom(1);
+        Mom mom2 = getMom(2);
+        Mom mom3 = getMom(3);
+        Mom mom4 = getMom(4);
+
+        //pull wine buttons
         final ImageButton p1 = (ImageButton)findViewById(R.id.wine1);
         final ImageButton p2 = (ImageButton)findViewById(R.id.wine2);
         final ImageButton p3 = (ImageButton)findViewById(R.id.wine3);
         final ImageButton p4 = (ImageButton)findViewById(R.id.wine4);
 
-        p1.setOnClickListener( new Button.OnClickListener(){
-            public void onClick(View view){
-                Intent i = new Intent(getApplicationContext(),PersonDetailActivity.class);
+        //set wine buttons to correct image
+        setMomImage(mom1, p1);
+        setMomImage(mom2, p2);
+        setMomImage(mom3, p3);
+        setMomImage(mom4, p4);
+
+        //Set on click listeners for all wine buttons
+        p1.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), PersonDetailActivity.class);
                 i.putExtra("MOM", "Annie");
                 startActivity(i);
             }
@@ -55,12 +69,37 @@ public class MainActivity extends Activity {
         });
     }
 
-    public void incr_glass(int current_image){
-        switch (current_image){
-            case R.drawable.wine_glass:
-                break;
+    public Mom getMom(int num){
+        return null;
+    }
+      
+    public void setMomImage(Mom m, ImageButton b){
+        switch (m.getPercent_full()){
+            case 0:
+                b.setBackgroundResource(R.drawable.wine_glass);
+            case 10:
+                b.setBackgroundResource(R.drawable.wine_glass);
+            case 20:
+                b.setBackgroundResource(R.drawable.wine_glass);
+            case 30:
+                b.setBackgroundResource(R.drawable.wine_glass);
+            case 40:
+                b.setBackgroundResource(R.drawable.wine_glass);
+            case 50:
+                b.setBackgroundResource(R.drawable.wine_glass);
+            case 60:
+                b.setBackgroundResource(R.drawable.wine_glass);
+            case 70:
+                b.setBackgroundResource(R.drawable.wine_glass);
+            case 80:
+                b.setBackgroundResource(R.drawable.wine_glass);
+            case 90:
+                b.setBackgroundResource(R.drawable.wine_glass);
+            case 100:
+                b.setBackgroundResource(R.drawable.wine_glass);
         }
     }
+
     public void incr_bottle(int current_image){
         switch (current_image){
             case R.drawable.wine_bottle:
