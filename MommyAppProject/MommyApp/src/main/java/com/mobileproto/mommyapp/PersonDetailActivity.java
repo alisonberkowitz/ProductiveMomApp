533b541722ed170369cb49550cd5a8fb66c5394a
Berko <alison.berkowitz@students.olin.edu>
2013-11-13 21:42:56 -0500

    fixing xml errors

--- MommyAppProject/MommyApp/src/main/java/com/mobileproto/mommyapp/PersonDetailActivity.java
+++ MommyAppProject/MommyApp/src/main/java/com/mobileproto/mommyapp/PersonDetailActivity.java
@@ -1,5 +1,6 @@
 package com.mobileproto.mommyapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -8,7 +9,7 @@ import android.widget.TextView;
 /**
  * Created by alison on 11/10/13.
  */
-public class PersonDetailActivity {
+public class PersonDetailActivity extends Activity{
 
     TextView personName;
 
@@ -18,6 +19,9 @@ public class PersonDetailActivity {
         setContentView(R.layout.activity_person_detail);
         Intent intent = getIntent();
 
+        final String name = intent.getExtras().getString("MOM");
+
         personName = (TextView)findViewById(R.id.personName);
+        personName.setText(name);
     }
 }
